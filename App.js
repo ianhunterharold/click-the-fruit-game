2db02614c1c937caf9d8586c697db89a50205d29
Brian Shaffer <brian@brianshaffer.com>
2020-01-14 09:59:49 -0600

    Refactored basket structure & styles from App.js into Basket.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-console.log('Starting...');
 import React, {Component} from 'react';
 import Coconut from './src/components/Coconut';
 import Basket from './src/components/Basket';
@@ -14,19 +13,15 @@ import {
   Dimensions
 } from 'react-native';
 
-// gather all images
+// gather images
 import bgImage from './src/img/background.png'
 import palm from './src/img/palm.png'
 import coconut from './src/img/coconut.png'
-//import coconut from './src/img/coconut-green.png'
-import basket from './src/img/basket.png'
 
 // get device dimentsion
 const {width: WIDTH, height: HEIGHT} = Dimensions.get('window')
-console.log(WIDTH, HEIGHT);
 
 export default class App extends Component{
-  
   render(){
     return (
       <ImageBackground source={bgImage} style={styles.backgroundContainer}>
@@ -38,23 +33,7 @@ export default class App extends Component{
           <InformationModal/>
           <Coconut handleClick={this.handleClickedCoconut}/>
           {/* passingdown my event listener via props might need to envoke it here */}
-
-          <View style={styles.basket}>
-            <Image source={basket} style={styles.basketImage} />
-            <View style={styles.basketCircle}>
-              <Basket/>
-              <Text style={styles.basketCount}></Text>
-            </View>
-            <View style={styles.basketCoconuts}>
-              <Image source={coconut} style={styles.basketCoconut1} />
-              <Image source={coconut} style={styles.basketCoconut2} />
-              <Image source={coconut} style={styles.basketCoconut3} />
-              <Image source={coconut} style={styles.basketCoconut4} />
-              <Image source={coconut} style={styles.basketCoconut5} />
-              <Image source={coconut} style={styles.basketCoconut6} />
-            </View>
-          </View>
-          {/* <Text>Game status goes here</Text> */}
+          <Basket />
         </View>
       </ImageBackground>
     )
@@ -73,12 +52,6 @@ const styles = StyleSheet.create({
     width: WIDTH - 55,
     height: HEIGHT,
   },
-  // palmTree: {
-  //   flex: 1,
-  //   width: undefined,
-  //   height: undefined,
-  //   resizeMode: 'contain',
-  // },
   palmTree: {
     position: 'absolute',
     width: '100%',
@@ -94,102 +67,5 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     left: 0
-  },
-  basket: {
-    width: '35%',
-    height: 70,
-    position: 'absolute',
-    bottom: 14,
-    right: 10,
-    alignItems: 'center',
-  },
-  basketCoconuts: {
-    position: 'absolute',
-    top: -30,
-    left: 0,
-    zIndex: 1,
-    width: '100%'
-  },
-  basketCoconut1: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 10,
-    zIndex: 3
-  },
-  basketCoconut2: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 0,
-    left: 22,
-    transform: [{ rotate: '10deg' }],
-    zIndex: 4
-  },
-  basketCoconut3: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 0,
-    left: 44,
-    transform: [{ rotate: '50deg' }],
-    zIndex: 3
-  },
-  basketCoconut4: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 8,
-    left: 66,
-    transform: [{ rotate: '15deg' }],
-    zIndex: 4
-  },
-  basketCoconut5: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 0,
-    left: 85,
-    transform: [{ rotate: '35deg' }],
-    zIndex: 3
-  },
-  basketCoconut6: {
-    width: 46,
-    height: 50,
-    position: 'absolute',
-    top: 5,
-    right: 0,
-    transform: [{ rotate: '70deg' }],
-    zIndex: 4
-  },
-  basketImage: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain',
-    zIndex: 2
-  },
-  basketCircle: {
-    width: 75,
-    height: 75,
-    borderRadius: 75/2,
-    backgroundColor: 'rgba(255, 255, 255, 0.85)',
-    borderColor: 'rgba(232, 90, 80, 1)',
-    borderStyle: 'solid',
-    borderWidth: 3,
-    position: 'absolute',
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: 'rgba(191, 114, 42, 0.7)',
-    shadowOffset: {
-      width: 0,
-      height: 1
-    },
-    shadowRadius: 2,
-    shadowOpacity: 1,
-    zIndex: 3
-  },
-  basketCount: {
-    fontSize: 20,
-    color: '#000'
   }
 })
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+// import images
+import basket from '../img/basket.png'
+import coconut from '../img/coconut.png'
 
 class Basket extends Component{
 
@@ -9,11 +13,123 @@ class Basket extends Component{
 
   render(){
     return(
-      <View>
-      <Text>{this.state.treeCoconutCount}</Text>
+      <View style={styles.basket}>
+        <Image source={basket} style={styles.basketImage} />
+        <View style={styles.basketCircle}>
+          <Text style={styles.basketCount}>{this.state.treeCoconutCount}</Text>
+        </View>
+        <View style={styles.basketCoconuts}>
+          <Image source={coconut} style={styles.basketCoconut1} />
+          <Image source={coconut} style={styles.basketCoconut2} />
+          <Image source={coconut} style={styles.basketCoconut3} />
+          <Image source={coconut} style={styles.basketCoconut4} />
+          <Image source={coconut} style={styles.basketCoconut5} />
+          <Image source={coconut} style={styles.basketCoconut6} />
+        </View>
       </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  basket: {
+    width: '35%',
+    height: 70,
+    position: 'absolute',
+    bottom: 14,
+    right: 10,
+    alignItems: 'center',
+  },
+  basketCoconuts: {
+    position: 'absolute',
+    top: -30,
+    left: 0,
+    zIndex: 1,
+    width: '100%'
+  },
+  basketCoconut1: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 10,
+    zIndex: 3
+  },
+  basketCoconut2: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 0,
+    left: 22,
+    transform: [{ rotate: '10deg' }],
+    zIndex: 4
+  },
+  basketCoconut3: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 0,
+    left: 44,
+    transform: [{ rotate: '50deg' }],
+    zIndex: 3
+  },
+  basketCoconut4: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 8,
+    left: 66,
+    transform: [{ rotate: '15deg' }],
+    zIndex: 4
+  },
+  basketCoconut5: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 0,
+    left: 85,
+    transform: [{ rotate: '35deg' }],
+    zIndex: 3
+  },
+  basketCoconut6: {
+    width: 46,
+    height: 50,
+    position: 'absolute',
+    top: 5,
+    right: 0,
+    transform: [{ rotate: '70deg' }],
+    zIndex: 4
+  },
+  basketImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    zIndex: 2
+  },
+  basketCircle: {
+    width: 75,
+    height: 75,
+    borderRadius: 75/2,
+    backgroundColor: 'rgba(255, 255, 255, 0.85)',
+    borderColor: 'rgba(232, 90, 80, 1)',
+    borderStyle: 'solid',
+    borderWidth: 3,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: 'rgba(191, 114, 42, 0.7)',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowRadius: 2,
+    shadowOpacity: 1,
+    zIndex: 3
+  },
+  basketCount: {
+    fontSize: 20,
+    color: '#000'
+  }
+});
 
 export default Basket;
